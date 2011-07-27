8c06f321dd413a1bed76d50ca6af225bf7a44a6b
Adrian <adrian.pillinger@gmail.com>
2011-07-27 21:52:43 +0100

    Trying out some ajax code

--- custom.js
+++ custom.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
-   $("#orderedlist").addClass("red");
+ $(document).ready(function() {
+ 	 $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+	  {
+		tags: "twitter",
+		tagmode: "any",
+		format: "json"
+	  },
+	  function(data) {
+		$.each(data.items, function(i,item){
+		  $("<img/>").attr("src", item.media.m).appendTo("#images");
+		  if ( i == 20 ) return false;
+		});
+	  });
  });
+ 
+
+  
+  //	 $.getJSON('ajax/test.json', function(data) {
+// 	  var items = [];
+// 	
+// 	  $.each(data, function(key, val) {
+// 		items.push('<li id="' + key + '">' + val + '</li>');
+// 	  });
+// 	
+// 	  $('<ul/>', {
+// 		'class': 'my-new-list',
+// 		html: items.join('')
+// 	  }).appendTo('body');
+//	});
