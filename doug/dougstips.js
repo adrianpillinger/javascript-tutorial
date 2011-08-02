5c104af18be3ce3675269bf74eb295522a13f182
Adrian <adrian.pillinger@gmail.com>
2011-08-02 22:58:32 +0000

    Auto complete text box added

--- doug/dougstips.js
+++ doug/dougstips.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         url: 'forms.json',
         success: function( data ) {
         	var questions = data['questions'];
+            var answerId;
             
             // form title
             $('#questions').append('<h1>' + data['formName'] + '</h1>');
@@ -12,9 +13,9 @@ $(document).ready(function() {
             {
             	var thisQuestion = questions[i];
                 $('#questions').append('<p/>' + thisQuestion["question"]);
-                if (thisQuestion["type"] === "ENUM")
+                if (thisQuestion["answerType"] === "ENUM")
                 {
-                	var answerId = "answer_"+i;
+                	answerId = "answer_"+i;
 	                $('#questions').append("<input id='"+answerId+"'></div>");
                 	$('#'+answerId).autocomplete({
 						source: thisQuestion["values"]
@@ -57,24 +58,24 @@ $(document).ready(function() {
 //    alert($.myApplication.getName());
 //});
 
-var dougstips = {};
-var initialS46 = {
-	type : "A",
-	lastLetterSentDate : null
-};
-
-$(document).ready(function() {
-	// adding typeAlert method to all objects
-	Object.prototype.typeAlert = function() {
-		alert("Type " + this.type);
-	};
-	if (typeof Object.create !== 'function') {
-		Object.create = function (o) {
-			var F = function () {};
-			F.prototype = o;
-			return new F();
-		};
-	}
+//var dougstips = {};
+//var initialS46 = {
+//	type : "A",
+//	lastLetterSentDate : null
+//};
+//
+//$(document).ready(function() {
+//	// adding typeAlert method to all objects
+//	Object.prototype.typeAlert = function() {
+//		alert("Type " + this.type);
+//	};
+//	if (typeof Object.create !== 'function') {
+//		Object.create = function (o) {
+//			var F = function () {};
+//			F.prototype = o;
+//			return new F();
+//		};
+//	}
 
 //	dougstips.applicationName = "Testing Doug's tips";
 //	alert(dougstips.applicationName);
@@ -86,5 +87,5 @@ $(document).ready(function() {
 //	alert(s46.type);
 //	alert(initialS46.type);
 //	s46.typeAlert();
-	
-});
+	 
+//});
